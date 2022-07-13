d622dcb9881f430ba835073b8942920b6a9dc63d
Sumiartini <Sumiartinisrirahayu@gmail.com>
2022-07-13 19:08:48 +0700

    latihan tekpro dari auth sampai hapus tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Task;
+use App\Repositories\TaskRepository;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    protected $tasks;
+    public function __construct(TaskRepository $tasks){
+        $this->middleware('auth');
+        $this->tasks = $tasks;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return view ('tasks.index',[
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect ('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        
+        $task->delete('destroy', $task);
+
+        return redirect('/tasks');
+    }
+}
